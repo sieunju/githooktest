02be9c2a785cb2896b4be2b886a440e9cd2c9a3a
sieunju <j.sieun73@gmail.com>
2025-08-08 23:33:18 +0900

    feat: 안드로이드 기본 셋팅 추가

---
+++ app/src/main/java/com/hmju/githooktest/MainApplication.kt
@@ -0,0 +1,12 @@
+package com.hmju.githooktest
+
+import android.app.Application
+
+/**
+ * Description :
+ *
+ * Created by juhongmin on 2025. 8. 8.
+ */
+class MainApplication : Application() {
+
+}
